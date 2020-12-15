5ff5a9131819bc9c1a30ce88d15124a0923879d6
lbrkanic <luka.brkanic1@gmail.com>
2020-12-15 19:50:34 +0100

    Encoder service test added

--- client/src/app/encoder.service.spec.ts
+++ client/src/app/encoder.service.spec.ts
@@ -1,16 +1,38 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 
 import { EncoderService } from './encoder.service';
 
-xdescribe('EncoderService', () => {
-  let service: EncoderService;
+describe('EncoderService', () => {
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(EncoderService);
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    let encoderService: EncoderService = TestBed.inject(EncoderService);
+    expect(encoderService).toBeTruthy();
+  });
+
+  it('should encode input message', () => {
+    let encoderService: EncoderService = TestBed.inject(EncoderService);
+    encoderService.encode('RRFA').subscribe((res) => {
+      expect(res).toEqual('R2F1A1');
+    });
+
+    const req = httpTestingController.expectOne('/ws/encoder');
+    expect(req.request.method).toBe('POST');
+    req.flush({ result: 'R2F1A1' });
   });
 });
